f3790deb6152cb2b08b6d2c3bcaa3d30e5dcfb07
yuligros <64066535+yuligros@users.noreply.github.com>
2020-05-11 14:05:32 +0300

    Update Employees.java

--- Fatma3/src/Employees.java
+++ Fatma3/src/Employees.java
@@ -1,14 +1,16 @@
 import java.util.Vector;
 
-abstract class Employees {
+abstract class Employees implements Comparable <Employees> {
 	private int id;
 	private String name;
 	private int age;
+	private double salary;
 
 	public Employees(int id, String name, int age) {
 		this.id = id;
 		this.name = name;
 		this.age = age;
+		this.salary=0;
 
 	}
 
@@ -23,12 +25,16 @@ abstract class Employees {
 	public int getAge() {
 		return this.age;
 	}
+
+	public double getSalary() {
+		return salary;
+	}
 	
-	abstract public long getSalary();
+	public double setSalary(double num) {
+		return salary +num ;
+	}
+	
+	abstract public void calculateSalary(Vector<OfflineOrders> orders,Vector <Custumers> custumers,Vector<Events> events);
 
-	abstract long calculateSalary(Vector <OfflineOrders> orders,Vector <Custumers> custumers);
 
-//	abstract int calculatebonus(Custumers custumer);
-
-//abstract long calculateSales();
 }
