d3724c4dddbbd588e95b03f62578d8c35e9279bc
yuligros <64066535+yuligros@users.noreply.github.com>
2020-05-12 11:44:06 +0300

    Update NegativePriceException.java

--- Fatma3/src/NegativePriceException.java
+++ Fatma3/src/NegativePriceException.java
@@ -1,4 +1,14 @@
+import java.io.IOException;
 
-public class NegativePriceException extends RuntimeException{
+public class NegativePriceException extends IOException{
+private String Message;
+	
+	public NegativePriceException() {
+		this.Message = "Price cannot be nagative";
+	}
+
+	public String getMessage() {
+		return Message;
+	}
 
 }
