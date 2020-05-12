f3f774a278e461c1cbd668fa639c8e444c6246ff
yuligros <64066535+yuligros@users.noreply.github.com>
2020-05-12 11:44:40 +0300

    Update WrongGenderInputException.java

--- Fatma3/src/WrongGenderInputException.java
+++ Fatma3/src/WrongGenderInputException.java
@@ -1,4 +1,14 @@
+import java.io.IOException;
 
-public class WrongGenderInputException extends RuntimeException  {
+public class WrongGenderInputException extends IOException {
+	private String Message;
+	
+	public WrongGenderInputException() {
+		this.Message = "Wrong gender, can be only 'm' or 'f' ";
+	}
+
+	public String getMessage() {
+		return Message;
+	}
 	
 }
